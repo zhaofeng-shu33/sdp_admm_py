ed0ceea24ec123f75a276a3cf4c6dbee813fab07
zhaofeng-shu33 <616545598@qq.com>
2021-04-20 10:57:58 +0800

    update meta info

--- setup.py
+++ setup.py
@@ -7,14 +7,21 @@ extensions = [
               include_dirs=[root_dir + 'eigen3']
             )
     ]
-setup(
-    name='sdp_admm_py',
-    ext_modules=cythonize(extensions),
-    version = '0.3',
-    author = 'zhaofeng-shu33',
-    author_email = '616545598@qq.com',
-    license = 'Apache License Version 2.0',
-    description = 'SBM community detection with semi-definite programming'
-)
+with open("README.md") as fh:
+    long_description = fh.read()
+
+if __name__ == '__main__':
+    setup(
+        name = 'sdp_admm_py',
+        ext_modules = cythonize(extensions),
+        url = 'https://github.com/zhaofeng-shu33/sdp_admm_py',
+        version = '0.3',
+        author = 'zhaofeng-shu33',
+        author_email = '616545598@qq.com',
+        long_description = long_description,
+        long_description_content_type="text/markdown",           
+        license = 'Apache License Version 2.0',
+        description = 'SBM community detection with semi-definite programming'
+    )
 
 
